04ebba645cd90cb614082ccaafec8b801ac25c51
aarsh21 <aarshpersonal21@gmail.com>
2024-08-13 21:11:34 +0530

    chore: Add example.env and change port number.
    - added example env variable for reference.
    - changed portnumber to 6565.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 
 const app = new Hono();
+const port = 6565;
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Hello To Open Exchange API!");
 });
 
-export default app;
+
+export default {
+  port,
+  fetch: app.fetch,
+};
